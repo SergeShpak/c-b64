f9e5be6aabecea9177fd6145df7cd94bc8fc8d7d
Sergey Shpak <serge1272@mail.ru>
2017-12-10 19:17:35 +0100

    chore(b64): remove tests from master

--- tests/include/predicates.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef B64_C_TESTS_PREDICATES_HPP
-#define B64_C_TESTS_PREDICATES_HPP
-
-#include "gtest/gtest.h"
-
-::testing::AssertionResult ArraysMatch(
-    uint8_t *expected, uint8_t *actual, size_t size);
-
-#endif
